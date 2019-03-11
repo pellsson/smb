f799ee6b6d48f09d4e94d96ee659c635ad44688a
pellsson <pellelindblad@gmail.com>
2019-03-11 19:13:15 +0100

    v4.1

--- nss/to_asm.py
+++ nss/to_asm.py
@@ -1,11 +1,14 @@
-nt = open('settings.nam', 'rb').read()
-row = []
-for i in range(0, len(nt)):
-	if 0 == (i & 0xff):
-		print('\nsettings_nt_data_%d:\n\t.byte ' % (i / 0x100), end='')
-	elif i and 0 == (i & 0xf):
-		print('\n\t.byte ', end='')
-	row.append('$%02X' % (nt[i]))
-	if 16 == len(row):
-		print(', '.join(row), end='')
-		row = []
+nts = [ 'nt', 'settings_nt' ]
+
+for name in nts:
+	nt = open('%s.nam' % (name), 'rb').read()
+	row = []
+	for i in range(0, len(nt)):
+		if 0 == (i & 0xff):
+			print('\n%s_data_%d:\n\t.byte ' % (name, i / 0x100), end='')
+		elif i and 0 == (i & 0xf):
+			print('\n\t.byte ', end='')
+		row.append('$%02X' % (nt[i]))
+		if 16 == len(row):
+			print(', '.join(row), end='')
+			row = []
